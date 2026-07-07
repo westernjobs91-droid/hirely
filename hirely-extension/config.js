a1bf4a1d09df1405f21ff1d7716f653d496a14ca
daycarecollaboration-tech <daycarecollaboration@gmail.com>
2026-07-07 00:17:57 -0400

    Restore Supabase anon key in taskpane.html and config.js

--- hirely-extension/config.js
+++ hirely-extension/config.js
@@ -5,6 +5,6 @@
 
 const HIRELY_CONFIG = {
   SUPABASE_URL: "https://rjxlnutucxxefgqtohnc.supabase.co",
-  SUPABASE_ANON_KEY: "PASTE_YOUR_SUPABASE_ANON_KEY_HERE",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqeGxudXR1Y3h4ZWZncXRvaG5jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI4NzIyNDMsImV4cCI6MjA5ODQ0ODI0M30.8GmNv0CjniF32bqKR335U8zBavnLDGN_ryodyT1RWJw",
   API_BASE: "https://app.hirelypro.com"
 };
